53ac03afaf410aa2a0a4e276345e99c906aea2c9
diegoambite <diegoambite@gmail.com>
2018-02-20 18:21:23 +0100

    Final version with extra part

---
+++ lab1/Debug/main.d
@@ -0,0 +1,7 @@
+main.d main.o: ../main.c ../trafo.h ../types.h ../lena512color.h
+
+../trafo.h:
+
+../types.h:
+
+../lena512color.h:
---
+++ lab1/trafo.h
@@ -0,0 +1,33 @@
+/*-----------------------------------------------------------------
+**
+**  Fichero:
+**    trafo.h  10/6/2014
+**
+**    Estructura de Computadores
+**    Dpto. de Arquitectura de Computadores y Autom�tica
+**    Facultad de Inform�tica. Universidad Complutense de Madrid
+**
+**  Prop�sito:
+**    Contiene las declaraciones de los prototipos de funciones
+**    usadas por el programa principal
+**
+**  Notas de dise�o:
+**
+**---------------------------------------------------------------*/
+
+#ifndef _TRAFO_H
+#define _TRAFO_H
+
+#include "types.h"
+
+void RGB2GrayMatrix(pixelRGB orig[], unsigned char dest[], int nfilas, int ncols); //done
+
+void Gray2BinaryMatrix(unsigned char orig[], unsigned char dest[], unsigned char umbral, int nfilas, int ncols);//done
+
+void apply_gaussian(unsigned char im1[], unsigned char im2[], int width, int height);//done
+
+void apply_sobel(unsigned char im1[], unsigned char im2[], int width, int height);//done
+
+int countWhite(unsigned char im[], int size);
+
+#endif
